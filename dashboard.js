c5b0de61e4f7b62746b8614194ad77e323b2ff4b
Subhan <msabbas.bscs20seecs@seecs.edu.pk>
2021-06-01 08:23:43 +0500

    Uploaded to hreoku

--- dashboard.js
+++ dashboard.js
@@ -8,10 +8,11 @@ module.exports.populate = async (req, res) => {
 
   queryObj.push({ "author.id": req.user._id });
   // Apply filter for last 1 day
+
   const time = new Date();
   const posts = await Post.find({ $or: queryObj });
   const filteredPosts = posts.filter(post => {
-    return post.time.getTime() - time.getTime() < 86400000;
+    return time.getTime() - post.time.getTime() < 86400000;
   });
 
   res.render("dashboard", {
